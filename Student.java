cabffddacb3154c264313a7b629ef370a3cd0b36
Rene Angelo Prado <152822689+RenePrado@users.noreply.github.com>
2024-05-05 20:51:37 +0800

    Student.java

---
+++ Student.java
@@ -0,0 +1,9 @@
+public class Student {
+    public void borrowBook(Resource book) {
+        book.borrow();
+    }
+
+    public void borrowJournal(Resource journal) {
+        journal.borrow();
+    }
+}
